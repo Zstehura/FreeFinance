37f3229e733bc4a65f2984904f0d952816b729f3
Zack <zack.stehura@gmail.com>
2022-01-31 15:46:47 -0500

    More work on adding UI Components for navigation

--- app/src/main/java/com/example/financefree/MainActivity.java
+++ app/src/main/java/com/example/financefree/MainActivity.java
@@ -1,11 +1,25 @@
 package com.example.financefree;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
+import androidx.navigation.NavGraph;
+import androidx.navigation.NavGraphNavigator;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+import android.view.Menu;
+
+import com.google.android.material.appbar.CollapsingToolbarLayout;
+import com.google.android.material.navigation.NavigationView;
+
+import java.lang.reflect.Array;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,12 +28,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Toolbar tb = findViewById(R.id.toolbar);
         NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
+        assert navHostFragment != null;
         NavController navController = navHostFragment.getNavController();
-//        AppBarConfiguration appBarConfiguration =
-//                new AppBarConfiguration.Builder(navController.getGraph())
-//                        .setDrawerLayout(R.id.drawer_layout)
-//                        .build();
-
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
+        NavigationUI.setupWithNavController(tb, navController, appBarConfiguration);
     }
 }
+
+/*
+setOf(
+                R.id.home_menu, R.id.profile_menu, R.id.settings_menu), drawerLayout)
+
+The method setupActionBarWithNavController automatically updates
+ */
