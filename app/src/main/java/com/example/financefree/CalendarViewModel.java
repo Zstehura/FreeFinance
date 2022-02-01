ee0fc93bc6fbd247e6356fcfed4cab9453bc8c23
Zack <zack.stehura@gmail.com>
2022-02-01 16:28:59 -0500

    Navigation nearly working, just needs content to test

---
+++ app/src/main/java/com/example/financefree/CalendarViewModel.java
@@ -0,0 +1,7 @@
+package com.example.financefree;
+
+import androidx.lifecycle.ViewModel;
+
+public class CalendarViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
--- app/src/main/java/com/example/financefree/MainActivity.java
+++ app/src/main/java/com/example/financefree/MainActivity.java
@@ -1,19 +1,23 @@
 package com.example.financefree;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
 import androidx.navigation.NavGraph;
 import androidx.navigation.NavGraphNavigator;
+import androidx.navigation.Navigation;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Toast;
 
 import com.google.android.material.appbar.CollapsingToolbarLayout;
 import com.google.android.material.navigation.NavigationView;
@@ -25,6 +29,14 @@ import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final int PROFILE_FRAG_ID = R.id.profileFragment;
+    public static final int HOME_FRAG_ID = R.id.homeFragment;
+    public static final int SETTINGS_FRAG_ID = R.id.settingsFragment;
+    public static final int RECPAY_FRAG_ID = R.id.recurringPaymentsFragment;
+
+
+    AppBarConfiguration appBarConfiguration;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,10 +45,44 @@ public class MainActivity extends AppCompatActivity {
         Toolbar tb = findViewById(R.id.toolbar);
         NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
         assert navHostFragment != null;
+        NavigationView navigationView = findViewById(R.id.nav_view);
         NavController navController = navHostFragment.getNavController();
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph())
+        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph())
                 .setOpenableLayout(findViewById(R.id.drawer_layout))
                 .build();
+        //NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(tb, navController, appBarConfiguration);
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case PROFILE_FRAG_ID:
+                        navController.navigate(PROFILE_FRAG_ID);
+                        return true;
+                    case HOME_FRAG_ID:
+                        navController.navigate(HOME_FRAG_ID);
+                        return true;
+                    case SETTINGS_FRAG_ID:
+                        navController.navigate(SETTINGS_FRAG_ID);
+                        return true;
+                    case RECPAY_FRAG_ID:
+                        navController.navigate(RECPAY_FRAG_ID);
+                        return true;
+                    default:
+                        return false;
+                }
+
+            }
+        });
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        NavController navController = Navigation.findNavController(this,R.id.nav_host_fragment);
+        return NavigationUI.onNavDestinationSelected(item, navController) || super.onOptionsItemSelected(item);
+    }
+
+
 }
