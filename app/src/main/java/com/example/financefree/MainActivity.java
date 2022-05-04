012abdfdc9d47ab6e23c00441d033fbf7f57a240
zds10 <97972294+zds10@users.noreply.github.com>
2022-05-04 12:26:29 -0400

    Loan calculator class done / tested

--- app/src/main/java/com/example/financefree/MainActivity.java
+++ app/src/main/java/com/example/financefree/MainActivity.java
@@ -10,7 +10,6 @@ import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
-
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
--- app/src/main/java/com/example/financefree/structures/LoanCalculator.java
+++ app/src/main/java/com/example/financefree/structures/LoanCalculator.java
@@ -1,45 +1,109 @@
 package com.example.financefree.structures;
 
-import com.example.financefree.database.entities.Loan;
+import android.util.Log;
 
-public final class LoanCalculator {
+import java.util.GregorianCalendar;
 
-    
-    public static double balanceOn(Loan loan, long date) {
-        double bal = loan.principle;
-        long cDate = loan.start_date;
-        while(cDate < date){
-            if(Frequency.occursOn(loan, cDate)){
-                bal *= (1 + (loan.apr / Frequency.paymentsPerYear(loan)));
-            }
+public class LoanCalculator {
 
-            cDate++;
-        }
+    // TODO: create tests
+    //      write more options
+    //      create fragment
+    //      allow user to adjust settings
+    //      already added in the chart library (https://weeklycoding.com/mpandroidchart/)
 
-        return bal;
+    private double principle;
+    private int numMonths;
+    private double apr;
+    private double payment;
+
+    public LoanCalculator(double principle, int numMonths,
+                          double apr, double payment) {
+        this.principle = principle;
+        this.apr = apr;
+        this.numMonths = numMonths;
+        this.payment = payment;
     }
 
-    public static double projectedBalanceOn(Loan loan, long date, double payments) {
-        long cDate = 0;
-        for(long l: loan.payments.keySet()){
-            if(l < date && l > cDate) cDate = l;
-        }
-
-        double bal = balanceOn(loan, cDate);
-
-        return bal;
+    public LoanCalculator() {
+        this(25000, 48, 0.03125, 250);
     }
 
-    public static double getPayment(Loan loan) {
-        double r = loan.apr;
-        double n = Frequency.paymentsPerYear(loan);
-        double t = loan.term_Length;
-        double p = loan.principle;
+    public void setPrinciple(double principle){this.principle = principle;}
+    public void setApr(double apr) { this.apr = apr; }
+    public void setNumMonths(int numMonths) { this.numMonths = numMonths; }
+    public void setPayment(double payment) { this.payment = payment; }
+    public void setNumMonths(long startDate, long endDate) {
+        long d = endDate - startDate;
+        double y = d / 365d;
+        numMonths = (int) Math.round(y * 12);
+    }
 
-        final double pow = Math.pow((1 + r), (n * t));
-        double r1n1 = pow - 1;
-        double r1rn = r * pow;
-        return (p / r1n1 / r1rn);
+    public double getBalanceAtMonth(int monthNum) {
+        if(monthNum == numMonths) return 0;
+        double bal = principle;
+
+        for(int i = 0; i < monthNum; i++) {
+            bal *= (1 + (apr / 12));
+            bal -= payment;
+        }
+        return roundNum(bal);
+    }
+
+    public int calcTermLen() {
+        // Formula:     log[ (PMT/i) / {(PMT/i) - PV ]
+        //          n =   -------------------------
+        //                      log(1 + i)
+
+        if(payment < (principle * (apr / 12)))
+            return -1;
+
+        double pmt = payment;
+        double pv = principle;
+        double i = apr / 12;
+
+        double t1 = Math.log((pmt / i) / ((pmt/i) - pv));
+        double t2 = Math.log(1 + i);
+        double nm = (t1 / t2);
+        numMonths = (int) Math.round(nm);
+        return numMonths;
+    }
+
+    public double calcPrinciple() {
+        // Formula =>   PV = (PMT/i) * [1 - (1 / (1+i)^n]
+        //                      t1          t2
+        double pmt = payment;
+        double i = apr / 12;
+        double n = numMonths;
+        double t1 = pmt / i;
+        double t2 = 1 - (1 / Math.pow(1+i, n));
+        principle = t1 * t2;
+        principle = roundNum(principle);
+        return principle;
+    }
+
+    public double calcPayment() {
+        // Formula =>   PMT = [PV*i * (1+i)^n] / [(1+i)^n - 1]
+        //                             t1                   t2
+        //  PV = Principle Value
+        //  PMT = Payment
+        //  r = apr / termsPerYear
+        //  i = Total payments (terms * termsPerYear)
+        double pv = principle;
+        double i = apr / 12;
+        double n = numMonths;
+        double t1 = pv * i * Math.pow(1+i, n);
+        double t2 = Math.pow(1+i, n) - 1;
+        payment = t1 / t2;
+        payment = roundNum(payment);
+        return payment;
+    }
+
+    private double roundNum(double num) {
+        double n = num * 100;
+        n = Math.round(n);
+        n /= 100;
+        return n;
     }
 
 }
